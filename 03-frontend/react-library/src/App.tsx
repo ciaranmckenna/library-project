754769883f8a5376e08a09a77510d30c86f096cf
ciaranmckenna <hello@ciaranmckenna.com>
2023-07-23 20:10:11 +0100

    ReturnBook component consumed by Carousel

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './Layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/NavbarAndFooter/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
