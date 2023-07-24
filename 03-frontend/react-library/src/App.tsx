f62c613c6657be0eed9b01fd9d3c4dd3370af812
ciaranmckenna <hello@ciaranmckenna.com>
2023-07-24 14:43:06 +0100

    Added Heros and LibraryServices components

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/NavbarAndFooter/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/NavbarAndFooter/HomePage/Carousel';
+import { Heros } from './layouts/NavbarAndFooter/HomePage/Heros';
+import { LibraryServices } from './layouts/NavbarAndFooter/HomePage/LibraryServices';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Navbar/>
       <ExploreTopBooks/>
       <Carousel/>
+      <Heros/>
+      <LibraryServices/>
     </div>
   );
 }
